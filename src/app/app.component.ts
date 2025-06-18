725393c0c8054594684323051e0bee9362020181
Your Name <mohamaduhadijatu@gmail.com>
2025-06-18 10:03:08 -0400

    fix: css styles for components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostlistComponent } from './components/postlist/postlist.component';
+import { NavbarComponent} from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
