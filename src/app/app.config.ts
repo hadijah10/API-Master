0e958c6fadebcf4277177e74612566baa09bebd8
Your Name <mohamaduhadijatu@gmail.com>
2025-06-20 13:14:39 -0400

    feat:add caching interceptor to intercept get request

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CachingInterceptorService } from './services/caching-interceptor.service';
 import { routes } from './app.routes';
 import { Authinterceptor } from './interceptors/authinterceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    provideHttpClient(withInterceptors([Authinterceptor]))]
+    provideHttpClient(withInterceptors([Authinterceptor]))],
+    
 };
