4a05f3441941341a0770855feadcca5a221fc469
Your Name <mohamaduhadijatu@gmail.com>
2025-06-20 13:02:36 -0400

    feat: add caching sevice to store cached responses

---
+++ src/app/services/cacheservice.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import {CacheEntry} from '../models/interfaces/cachemodel'
+
+const MAX_CACHE_AGE = 300000; // Cache duration in milliseconds (e.g., 5 minutes)
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RequestCacheService {
+  private cache = new Map<string, CacheEntry>();
+
+  get(url: string): HttpResponse<any> | null {
+    const cached = this.cache.get(url);
+
+    if (!cached) {
+      return null;
+    }
+
+    const isExpired = (Date.now() - cached.entryTime) > MAX_CACHE_AGE;
+    if (isExpired) {
+      this.cache.delete(url);
+      return null;
+    }
+
+    return cached.response;
+  }
+
+  put(url: string, response: HttpResponse<any>): void {
+    const entry: CacheEntry = { url, response, entryTime: Date.now() };
+    this.cache.set(url, entry);
+  }
+}
